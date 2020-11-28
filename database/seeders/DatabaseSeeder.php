ae02d74b74ac17c751fccd93ab2a5987aa1746bd
Patrick <iipajcii@gmail.com>
2020-11-28 11:14:41 -0500

    Disabled (commeneted out) WebsiteSeeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(WebsiteSeeder::class);
+        // $this->call(WebsiteSeeder::class);
         $this->call(UserSeeder::class);
     }
 }
